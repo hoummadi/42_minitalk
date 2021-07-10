fcc733a605d4b71fe71832dbd164d6c0243cfc8b
Moulay Hoummadi <43641225+hoummadi@users.noreply.github.com>
2021-07-10 02:10:55 +0100

    done

--- ft_client.c
+++ ft_client.c
@@ -12,6 +12,12 @@
 
 #include <signal.h>
 
+void	kill_faild(void)
+{
+	ft_putstr("Failed to send message, check server pid!\n");
+	exit(1);
+}
+
 void	start_send(int pid_serv, char *to_send)
 {
 	int	bit;
@@ -26,9 +32,13 @@ void	start_send(int pid_serv, char *to_send)
 		while (bit < 8)
 		{
 			if ((to_send[i] >> bit) & 1)
-				kill(pid_serv, SIGUSR2);
+			{
+				if (kill(pid_serv, SIGUSR2) == -1)
+					kill_faild();
+			}
 			else
-				kill(pid_serv, SIGUSR1);
+				if (kill(pid_serv, SIGUSR1) == -1)
+					kill_faild();
 			bit++;
 			usleep(100);
 		}
@@ -38,13 +48,15 @@ void	start_send(int pid_serv, char *to_send)
 
 int	main(int ac, char **av)
 {
-	int	pid;// ft_atoi ft_strlen ft_putstr ft_putchar
+	int	pid;// ft_atoi ft_strlen ft_putstr ft_putchar ft_itoa
+	int	cli_pid;
 
 	if (ac != 3)
 	{
 		ft_putstr("The client takes as parameters : server_pid msg_to_send\n");
 		return (1);
 	}
+	cli_pid = getpid();
 	pid = ft_atoi(av[1]);// check atoi
 	if (pid < 0 || pid > 99998)
 	{
@@ -52,7 +64,13 @@ int	main(int ac, char **av)
 		return (1);
 	}
 	else
-		start_send(pid, av[2]);		
+	{
+		start_send(pid, "this message from ");
+		start_send(pid, ft_itoa(cli_pid));
+		start_send(pid, " :\n");
+		start_send(pid, av[2]);
+	}
+	ft_putstr("Message sent successfully\n");
 	return (0);
 
 }
