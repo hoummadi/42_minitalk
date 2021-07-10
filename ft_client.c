6fc75be5e12ff1d421f7cea802902ff5a588671c
Moulay Hoummadi <43641225+hoummadi@users.noreply.github.com>
2021-07-10 01:02:46 +0100

    Update ft_client.c

--- ft_client.c
+++ ft_client.c
@@ -12,12 +12,47 @@
 
 #include <signal.h>
 
+void	start_send(int pid_serv, char *to_send)
+{
+	int	bit;
+	int	len;
+	int	i;
+
+	i = 0;
+	len = ft_strlen(to_send);
+	while (i <= len)
+	{
+		bit = 0;
+		while (bit < 8)
+		{
+			if ((to_send[i] >> bit) & 1)
+				kill(pid_serv, SIGUSR1);
+			else
+				kill(pid_serv, SIGUSR2);
+			bit++;
+			usleep(100);
+		}
+		i++;
+	}
+}
+
 int	main(int ac, char **av)
 {
+	int	pid;// ft_atoi ft_strlen ft_putstr ft_putchar
+
 	if (ac != 3)
 	{
-		// required pid sms
-		// close
+		ft_putstr("The client takes as parameters : server_pid msg_to_send\n");
+		return (1);
 	}
+	pid = ft_atoi(av[1]);// check atoi
+	if (pid < 0 || pid > 99998)
+	{
+		ft_putstr("Invalide server_pid\n");
+		return (1);
+	}
+	else
+		start_send(pid, av[2]);		
+	return (0);
 
 }
